2590c7edbd859e7123fcd34565b4f6efc76b7c69
Tedi <tedi.codewithme.com>
2026-08-03 07:49:38 -0700

    deleted the old version

--- src/src/utils/helpers.ts
+++
@@ -1,7 +0,0 @@
-export const cn = (...classes: (string | boolean | undefined | null)[]): string => {
-  return classes.filter(Boolean).join(' ')
-}
-
-export const generateId = (): string => {
-  return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15)
-}
